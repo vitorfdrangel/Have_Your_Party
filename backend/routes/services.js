485851457fdb87e7ce05efc5ddb3e3db5afef99b
Vitor Rangel <vitorfdrangel@gmail.com>
2024-07-11 20:24:20 -0300

    Backend_Party_Time

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -16,4 +16,8 @@ router
   .route("/services/:id")
   .delete((req, res) => serviceController.delete(req, res));
 
+router
+  .route("/services/:id")
+  .put((req, res) => serviceController.update(req, res));
+
 module.exports = router;
