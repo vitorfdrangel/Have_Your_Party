c21b5383fca33505ca8714472640a4c43a59f5d4
Vitor Rangel <vitorfdrangel@gmail.com>
2024-07-16 23:32:21 -0300

    Backend_Party_Time

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -72,6 +72,35 @@ const partyController = {
 
     res.status(200).json({ deletedParty, msg: "Festa excluída com sucesso!" });
   },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      };
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente." });
+        return;
+      }
+
+      const updateParty = await PartyModel.findByIdAndUpdate(id, party);
+
+      if (!updateParty) {
+        res.status(404).json({ msg: "Festa não encontrada." });
+      }
+
+      res.status(200).json({ party, msg: "Festa atualizada com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = partyController;
