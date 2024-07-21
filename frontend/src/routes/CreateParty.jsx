f5444971d97a2d869c90d58d2cd448206ee1d403
Vitor Rangel <vitorfdrangel@gmail.com>
2024-07-20 23:55:32 -0300

    HYP

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import partyFetch from "../axios/config";
+
+import { useState, useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import "./Form.css";
 
 const CreateParty = () => {
+  const [services, setServices] = useState([]);
+
+  // Load Services
+  useEffect(() => {
+    const loadServices = async () => {
+      try {
+        const res = await partyFetch.get("/services");
+
+        setServices(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    loadServices();
+  }, []);
+
   return (
     <div className="form-page">
       <h2>Crie sua próxima Festa</h2>
@@ -40,7 +63,19 @@ const CreateParty = () => {
         <div>
           <h2>Escolha os serviços</h2>
           <div className="services-container">
-            <p>Serviços...</p>
+            {services.length === 0 && <p>Carregando...</p>}
+            {services.length != 0 &&
+              services.map((service) => (
+                <div className="service" key={service._id}>
+                  <img src={service.image} alt={service.name} />
+                  <p className="service-name">{service.name}</p>
+                  <p className="service-price">R${service.price}</p>
+                  <div className="checkbox-container">
+                    <input type="checkbox" value={service._id} />
+                    <p>Marque para solicitar</p>
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
         <input type="submit" value="Criar Festa" className="btn" />
