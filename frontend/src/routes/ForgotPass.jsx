6a1a6a306bc443f419a217776fabed20b6b547e2
Vitor Rangel <vitorfdrangel@gmail.com>
2024-07-31 22:50:29 -0300

    Criando forgot pass

--- frontend/src/routes/ForgotPass.jsx
+++ frontend/src/routes/ForgotPass.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import "./ForgotPass.css";
 
 const ForgotPass = () => {
-  return <div>ForgotPass</div>;
+  return (
+    <div className="forgot-pass">
+      <h2>Redefinição de senha!</h2>
+      <p>
+        Informe seu email e enviaremos um link para a recuperação da sua senha
+      </p>
+      <form>
+        <label>
+          <span>Email</span>
+          <input type="email" placeholder="Digite seu email" required />
+        </label>
+        <input
+          type="submit"
+          value="Enviar link de recuperação"
+          className="btn-secondary"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default ForgotPass;
